82f7d2e6aab5e7d43747375cc43eb9459c9d7d22
ratandheeraj <ratandheeraj1@gmail.com>
2022-01-14 18:19:59 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <h1 class="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+      <button class="btn btn-primary">daisyUI Button</button>
+    </div>
+  )
+}
+
+export default App
